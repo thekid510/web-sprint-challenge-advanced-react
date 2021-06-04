dbdbd2036e260687ef770d274fa876877c47b2cc
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-06-04 14:03:59 -0700

    building out tests

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
-
+import {render, screen, waitFor } from '@testing-library/react';
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    const header = screen.queryByText(/Checkout Form/i)
+    expect(header).toHaveTextContent(/Checkout Form/i)
 
-test("form shows success message on submit with form details", () => {});
+});
+
+test("form shows success message on submit with form details", () => {
+
+
+
+
+});
